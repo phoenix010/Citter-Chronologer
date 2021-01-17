e93c2b425ce2f01c99e51af618547a921877d56b
phoenix010 <tyagiaditya62@gmail.com>
2021-01-16 21:23:48 -0600

    CHANGES

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/impl/CustomerServiceImpl.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,23 @@
 package com.udacity.jdnd.course3.critter.service.impl;
 
+import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.service.CustomerService;
 
+import java.util.List;
+
 public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Customer findById(long customerId) {
+        return null;
+    }
 }
